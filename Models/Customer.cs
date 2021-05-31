3a99ae4876e7fa7baffbefcd8616a9134f75d564
huyem <huyem1004@gmail.com>
2021-05-31 23:53:11 +0700

    huy

---
+++ Models/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baitapnhom.Models
+{
+    class Customer
+    {
+       
+        private string name;
+        private string address;
+        private string phone;
+        private string age;
+        private string type;
+
+        public Customer()
+        {
+        }
+
+        public Customer(string name, string address, string phone, string age, string type)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+            Age = age;
+            Type = type;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Address { get => address; set => address = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Age { get => age; set => age = value; }
+        public string Type { get => type; set => type = value; }
+    }
+}
--- Pages/fromAddCustomer.xaml.cs
+++ Pages/fromAddCustomer.xaml.cs
@@ -13,7 +13,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Baitapnhom.Models;
-
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Baitapnhom.Pages
@@ -21,47 +20,37 @@ namespace Baitapnhom.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Customers : Page
+    public sealed partial class fromAddCustomer : Page
     {
-        public Customers()
+        public fromAddCustomer()
         {
-
             this.InitializeComponent();
-            contentCustomer.Navigate(typeof(Pages.ListCustomer));
-       
         }
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             string msg = e.Parameter as string;
-           
+            
+
+
+
 
         }
-
-       
-
-        private void btn1_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            contentCustomer.Navigate(typeof(ListCustomer));
+          
         }
 
-        private void btn2_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            contentCustomer.Navigate(typeof(fromAddCustomer), contentCustomer );
-        }
-
-      
-
-        private void btn3_Click(object sender, RoutedEventArgs e)
-        {
-            if (contentCustomer.CanGoBack)
-            {
-                contentCustomer.GoBack();
-            }
-        }
-
-        private void btn4_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+            string name = input1.Text;
+            string address = input2.Text;
+            string phone = input3.Text;
+            string age = input4.Text;
+            string type = input5.Text;
+            Customer customer = new Customer(name, address, phone, age, type);
+            this.Frame.Navigate(typeof(ListCustomer),customer);
+        
+                }
     }
 }
